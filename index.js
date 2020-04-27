2ef90dfb84643fd5f61934fd3a6f4ab72dd563ad
Hayden Bruin <bruinhayden@gmail.com>
2020-04-27 23:25:59 +1200

    Adding commands, setting up loot tracker

--- index.js
+++ index.js
@@ -6,21 +6,28 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+let loot = [];
+
 client.on('message', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'loot') {
-		message.channel.send('-- Adding Loot --');
-		message.channel.send('');
-		message.channel.send('');
-		message.channel.send('');
-		message.channel.send('');
-	}
-  
-  message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+  if (command === 'loottracker') {
+		message.channel.send(`-- Loot Tracker - Commands --`);
+		message.channel.send(`- !loot [Loot Worth (K)] IE: (!loot 560K)`);
+  }
+	else if (command === 'loot') {
+		message.channel.send(`-- Adding Loot --`);
+		message.channel.send(`Looting bag for ${message.author}`);
+    
+    loot[message.author]['total'] += args[0];
+    message.channel.send(`Loot Total: ${loot[message.author]['total']}`);
+  }
+  else if(command === 'endtrip') {
+		message.channel.send(`-- Ending Trip --`);
+  }
 });
 
 
